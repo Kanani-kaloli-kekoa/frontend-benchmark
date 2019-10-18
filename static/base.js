aa8f312a899047b22cb898f442a037d1ab839267
Student <student@dontation.local>
2019-10-18 15:13:36 -0500

    linking the pages. FInally figured it out.

--- static/base.js
+++ static/base.js
@@ -1,11 +1,10 @@
-//const button = document.querySelector("#likes");
+const button = document.querySelector("#likes");
+const listOfLikes = document
+button.addEventListener("click", () => {
+    const url = `entries/entry4/like?=${button.value}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
-
-//button.addEventListener("click", () => {
-    //const url = `entries/entry4/like?=${button.value}`
-    //fetch(url)
-       // .then(response => response.json())
-      //ss  .then(data => {
-
-      //  })
-//})
+        })
+})
