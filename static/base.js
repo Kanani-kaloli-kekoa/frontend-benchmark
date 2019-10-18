372eccc8f73184507fcf91b015df6589354f2f54
Student <student@dontation.local>
2019-10-18 13:22:23 -0500

    correcting the content code. updating javascript

---
+++ static/base.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("#likes");
+
+button.addEventListener("click", () => {
+    const url = `entries/entry4/like?=${button.value}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+        })
+})
